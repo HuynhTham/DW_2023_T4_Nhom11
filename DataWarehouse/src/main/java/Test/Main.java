e9f3465677aa351d5cf3abe6d6ecd44f06a26166
HuynhTham <108994252+HuynhTham@users.noreply.github.com>
2023-12-15 22:15:44 +0700

    Extract data from source

---
+++ DataWarehouse/src/main/java/Test/Main.java
@@ -0,0 +1,12 @@
+package Test;
+
+import Extract.module.ExtractData;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new ExtractData().extractData("xosohomnay");
+    }
+
+}
